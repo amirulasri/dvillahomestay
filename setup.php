c4faf7317ec7b274b44039ab7b4ae87d51c7209d
amirulasri <amirulasrix@gmail.com>
2021-04-15 18:21:24 +0800

    First Release

---
+++ setup.php
@@ -0,0 +1,17 @@
+<?php
+//error_reporting(E_ERROR | E_PARSE);
+
+$servername = "localhost";
+$username = "root";  
+$password = "";
+$dbname = "dvillahomestay";
+
+//-------------------------
+
+//Penyambungan ke pelayan. JANGAN USIK.
+$conn = new mysqli($servername, $username, $password, $dbname);
+$host = mysqli_connect($servername,$username,$password,$dbname);
+
+//Penyambungan untuk CSV EKSPORT/IMPORT dan Login Access. JANGAN USIK.
+$connect = mysqli_connect("$servername", "$username", "$password", "$dbname");
+?>
